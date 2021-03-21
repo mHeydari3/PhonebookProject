85476831596032f897bd64737292d992b003f2f6
mzeus98 <mzeus98@gmail.com>
2021-03-21 23:25:59 +0330

    Added connection and construction for dbClass.php

--- dbClass.php
+++ dbClass.php
@@ -2,11 +2,31 @@
 class db{
 
     protected $pdo;
-
-    public function __construct($server = 'localhost' , $db = 'phonebook' , $user='root' , $pass='')
+    private $database;
+    private $user;
+    private $password;
+    public function __construct( $database = 'php_phonebook' , $user='root' , $password='')
     {
-
+        $this->database=$database;
+        $this->user = $user;
+        $this->password = $password;
+        $this->connection();
 
 
     }
+
+    public function connection(){
+        try{
+            $this->pdo = new PDO("mysql:host=localhost;dbname={$this->database}", $this->user   , $this->password);
+        }
+        catch (Exception $e) {
+            die($e->getMessage());
+        }
+
+    }
+
+
+
 }
+
+$dbObj = new db();
