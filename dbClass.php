99f6ddd9b7f80169d9eb78170ed036aabbd5bd29
mzeus98 <mzeus98@gmail.com>
2021-03-22 16:33:42 +0430

    1.Added editdata function (based on pdo) 2.Added insertsampledata function 3.Added insert function (based on pdo ) 4.Added select data function (based on pdo)
    5.Added set_tbl function

--- dbClass.php
+++ dbClass.php
@@ -5,6 +5,7 @@ class db{
     private $database;
     private $user;
     private $password;
+    private $tbl;
     public function __construct( $database = 'php_phonebook' , $user='root' , $password='')
     {
         $this->database=$database;
@@ -18,6 +19,7 @@ class db{
     public function connection(){
         try{
             $this->pdo = new PDO("mysql:host=localhost;dbname={$this->database}", $this->user   , $this->password);
+
         }
         catch (Exception $e) {
             die($e->getMessage());
@@ -25,8 +27,79 @@ class db{
 
     }
 
+    public function setTbl($tbl_name){
+        $this->tbl = $tbl_name;
+    }
 
+    public function selectData($name){
+        if (is_array($name)){
+            $names =  implode("," , $name )  ;
+        }
+        $stm = $this->pdo->prepare("SELECT $names FROM {$this->tbl}");
+        $stm->execute();
+        $row = $stm->fetchAll(PDO::FETCH_OBJ);
+        return $row;
+
+    }
+
+    public function insertData($fields,$data){
+        if(is_array($data)){
+            $names= "'" . implode("','"  ,  $data  ) . "'";
+            $fields = implode (","  , $fields);
+            $sql = $this->pdo->prepare("INSERT INTO {$this->tbl} ($fields) VALUES ($names)");
+            $result_of_insert = $sql->execute();
+            if ($result_of_insert) { return true;}
+            else {return false;}
+        }
+
+    }
+
+    public function editData($fields , $data , $id){
+
+        foreach ($fields as $key=>$val){
+            $txt[] = $val . "='" . $data[$key]  . "'" ;
+
+        }
+        $query = implode("," , $txt);
+        $sql  = $this->pdo->prepare("UPDATE {$this->tbl} SET " . $query . "WHERE id ='$id'");
+        $res = $sql->execute();
+        return $res;
+    }
+
+
+    /*public function insertSampleData(){
+        try{
+            $this->pdo = new PDO("mysql:host=localhost;dbname=php_phonebook", "root"   , "");
+            $query = $this->pdo->prepare("INSERT INTO users_tbl (name,email,lastname,pasword) VALUES (:name,:email,:lastname,:password)");
+            $name = "mohammad";
+            $email="m2@gmail.2com";
+            $lastname = "heydari";
+            $password = "123";
+            $query->bindParam("name" , $name);
+            $query->bindParam("email" , $email);
+            $query->bindParam("lastname" ,$lastname );
+            $query->bindParam("password",$password);
+            $result = $query->execute();
+            var_dump($result);
+
+        }
+        catch (Exception $e){
+            die("ERROR MESSAGE:" . $e->getMessage());
+        }
+    }*/
 
 }
 
 $dbObj = new db();
+$dbObj->setTbl("users_tbl");
+$result_of_insert = $dbObj->insertData(["name" , "lastname" , "email" , "pasword"] , ["sarah" , "latif" , "sarah".rand()."@gmail.com" , "1234"]);
+if ($result_of_insert ) {
+    echo "<br/>INSERTED SUCCESSFULLY<br/>";
+
+    $selected_row = $dbObj->selectData(["email"]);
+    echo "<pre>"; print_r($selected_row) ;  echo "</pre> <br/><br/><br/><hr/>";
+}
+
+$res = $dbObj->editData(['name' , 'lastname' , 'email' , 'pasword'] , ['hossein koskhol' , 'kos nanaehamid' , 'e' .rand() .'@e.com' ,'pass'  ] , 20);
+
+echo "<pre>"; print_r($res) ;  echo "</pre>";
