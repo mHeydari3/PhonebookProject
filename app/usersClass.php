b497d4d992306aea939c31fc0f7c5032a9fa4b5d
mzeus98 <mzeus98@gmail.com>
2021-03-23 16:34:41 +0430

    Added logout method.

--- app/usersClass.php
+++ app/usersClass.php
@@ -1,11 +1,25 @@
 <?php
 include_once 'dbClass.php';
 class users extends db{
+
     protected $tbl = 'users_tbl';
     public function login($data){
-        $email = $data['email'];
+        $email    = $data['email'];
         $password = $data['password'];
         $this->setTbl($this->tbl);
-        $this->searchData($email , $email);
+        $user_data = $this->searchData('email' , $email);
+        //echo "<pre>". var_dump($a)."</pre>";
+        if($password == $user_data->pasword){
+            $_SESSION['name'] = $user_data->name;
+
+            header('location:dashboard.php');
+        }
     }
+
+    public function logout(){
+        session_destroy();
+        header('location:index.php?logout=ok');
+    }
+
+
 }
