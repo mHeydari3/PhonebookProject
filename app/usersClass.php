0ac1cb20d76643623945f3ca4eeb0e2fb993756d
mzeus98 <mzeus98@gmail.com>
2021-03-23 15:20:26 +0430

    Added login method

--- app/usersClass.php
+++ app/usersClass.php
@@ -1,5 +1,11 @@
 <?php
 include_once 'dbClass.php';
 class users extends db{
-
+    protected $tbl = 'users_tbl';
+    public function login($data){
+        $email = $data['email'];
+        $password = $data['password'];
+        $this->setTbl($this->tbl);
+        $this->searchData($email , $email);
+    }
 }
